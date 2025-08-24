409a8100f3b53a6959b2a5196db83a73a1f9496b
ProfessorFichte <70310161+ProfessorFichte@users.noreply.github.com>
2025-08-24 02:26:53 +0200

    Prevent Sound Server Crash

--- src/main/java/net/forcemaster_rpg/sounds/ModSounds.java
+++ src/main/java/net/forcemaster_rpg/sounds/ModSounds.java
@@ -1,34 +1,76 @@
 package net.forcemaster_rpg.sounds;
 
-import net.minecraft.client.render.entity.model.BreezeEntityModel;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static net.forcemaster_rpg.ForcemasterClassMod.MOD_ID;
 
 public class ModSounds {
-    public static final Identifier KNUCKLE_ATTACK_ID = Identifier.of(MOD_ID, "knuckle_attack");
-    public static SoundEvent KNUCKLE_ATTACK_EVENT = SoundEvent.of(KNUCKLE_ATTACK_ID);
-    public static final Identifier ASAL_RELEASE_ID = Identifier.of(MOD_ID, "asal_release");
-    public static SoundEvent ASAL_RELEASE_EVENT = SoundEvent.of(ASAL_RELEASE_ID);
-    public static final Identifier STONEHAND_CAST_ID = Identifier.of(MOD_ID, "stonehand_cast");
-    public static SoundEvent STONEHAND_CAST_EVENT = SoundEvent.of(STONEHAND_CAST_ID);
-    public static final Identifier KNUCKLE_SPELL_CAST_ID = Identifier.of(MOD_ID, "knuckle_spell_cast");
-    public static SoundEvent KNUCKLE_SPELL_CAST_EVENT = SoundEvent.of(KNUCKLE_SPELL_CAST_ID);
-    public static final Identifier KNUCKLE_SPELL_IMPACT_ID = Identifier.of(MOD_ID, "knuckle_spell_impact");
-    public static SoundEvent KNUCKLE_SPELL_IMPACT_EVENT = SoundEvent.of(KNUCKLE_SPELL_IMPACT_ID);
-    public static final Identifier SONIC_HAND_ID = Identifier.of(MOD_ID, "sonic_hand");
-    public static SoundEvent SONIC_HAND_EVENT = SoundEvent.of(SONIC_HAND_ID);
+    public static class Entry {
+        private final Identifier id;
+        private final SoundEvent soundEvent;
+        private RegistryEntry<SoundEvent> entry;
+        private int variants = 1;
+
+        public Entry(Identifier id, SoundEvent soundEvent) {
+            this.id = id;
+            this.soundEvent = soundEvent;
+        }
+
+        public Entry(String name) {
+            this(Identifier.of(MOD_ID, name));
+        }
+
+        public Entry(Identifier id) {
+            this(id, SoundEvent.of(id));
+        }
+
+        public Entry travelDistance(float distance) {
+            return new Entry(id, SoundEvent.of(id, distance));
+        }
+
+        public Entry variants(int variants) {
+            this.variants = variants;
+            return this;
+        }
+
+        public Identifier id() {
+            return id;
+        }
+
+        public SoundEvent soundEvent() {
+            return soundEvent;
+        }
+
+        public RegistryEntry<SoundEvent> entry() {
+            return entry;
+        }
+
+        public int variants() {
+            return variants;
+        }
+    }
+    public static final List<Entry> entries = new ArrayList<>();
+    public static Entry add(Entry entry) {
+        entries.add(entry);
+        return entry;
+    }
+    public static final Entry KNUCKLE_ATTACK = add(new Entry("knuckle_attack"));
+    public static final Entry ASAL_RELEASE = add(new Entry("asal_release"));
+    public static final Entry STONEHAND_CAST = add(new Entry("stonehand_cast"));
+    public static final Entry KNUCKLE_SPELL_CAST = add(new Entry("knuckle_spell_cast"));
+    public static final Entry KNUCKLE_SPELL_IMPACT = add(new Entry("knuckle_spell_impact"));
+    public static final Entry SONIC_HAND = add(new Entry("sonic_hand"));
 
     public static void register() {
-        Registry.register(Registries.SOUND_EVENT, KNUCKLE_ATTACK_ID, KNUCKLE_ATTACK_EVENT);
-        Registry.register(Registries.SOUND_EVENT, ASAL_RELEASE_ID, ASAL_RELEASE_EVENT);
-        Registry.register(Registries.SOUND_EVENT, STONEHAND_CAST_ID, STONEHAND_CAST_EVENT);
-        Registry.register(Registries.SOUND_EVENT, KNUCKLE_SPELL_CAST_ID, KNUCKLE_SPELL_CAST_EVENT);
-        Registry.register(Registries.SOUND_EVENT, KNUCKLE_SPELL_IMPACT_ID, KNUCKLE_SPELL_IMPACT_EVENT);
-        Registry.register(Registries.SOUND_EVENT, SONIC_HAND_ID, SONIC_HAND_EVENT);
-
+        for (var entry: entries) {
+            entry.entry = Registry.registerReference(Registries.SOUND_EVENT, entry.id(), entry.soundEvent());
+        }
     }
 }
