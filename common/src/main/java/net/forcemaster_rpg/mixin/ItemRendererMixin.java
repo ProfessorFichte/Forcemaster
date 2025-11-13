0e7b169d670ddab65af052706e5e2273ddafcc37
ProfessorFichte <70310161+ProfessorFichte@users.noreply.github.com>
2025-11-13 22:07:21 +0100

    Render Knuckle Inventory Texture also in Item Frames

--- common/src/main/java/net/forcemaster_rpg/mixin/ItemRendererMixin.java
+++ common/src/main/java/net/forcemaster_rpg/mixin/ItemRendererMixin.java
@@ -19,7 +19,7 @@ import static net.forcemaster_rpg.ForcemasterClassMod.MOD_ID;
     public class ItemRendererMixin {
         @ModifyVariable(method = "renderItem", at = @At(value = "HEAD"), argsOnly = true)
         public BakedModel useItemModel(BakedModel value, ItemStack stack, ModelTransformationMode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
-            if(stack.isIn(ModItemTags.KNUCKLES) && renderMode != ModelTransformationMode.GUI && renderMode != ModelTransformationMode.GROUND) {
+            if(stack.isIn(ModItemTags.KNUCKLES) && renderMode != ModelTransformationMode.GUI && renderMode != ModelTransformationMode.GROUND && renderMode != ModelTransformationMode.FIXED) {
                 String name = stack.getTranslationKey();
                 String name2 = name.toString().replace("item.forcemaster_rpg.","");
                 return ((ItemRendererAccessor)this).forcemaster$getModels().getModelManager().getModel(ModelIdentifier.ofInventoryVariant(Identifier.of(MOD_ID, name2 + "_model")));
